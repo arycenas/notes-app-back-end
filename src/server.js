e5da3549c52ab4cd8bd74075de2bad9cfc67bfcf
arycenas <672021189@student.uksw.edu>
2025-01-14 11:59:53 +0700

    port bug

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    host: process.env.HOST,
+    // host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
